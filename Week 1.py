15871832ec9df6cf605c2f692463f2b64907c662
Marcus Herstik <marcus@herstik.com>
2024-01-24 12:42:21 +1000

    Create Week 1.py

---
+++ Week 1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 24 10:42:30 2024
+
+@author: marcus
+"""
+
+######
+#
+#  get a persons name and say hello
+#
+#####
+# a = 3
+# b = 5.2
+# c = a - b
+# name = "Marcus"
+
+# print(name)
+
+# name = input("Give me your name: ")
+
+# print(a * name)
+
+# print(a + b)
+
+# print(name, "was here")
+
+
+## Challenge
+# ask a person their name.
+name = input("What is your name? ")
+# Say hello and their name.
+print("Hello", name)
+# ask if they want tea or coffee + how many sugars or milk
+drink = input("What drink do you want - coffee or tea? ")
+print(drink)
+# repeat their order back to them
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
