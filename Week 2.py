277ccb8be302bd3f3dcff3c7e28278348b092a6c
Marcus Herstik <marcus@herstik.com>
2024-01-31 09:52:23 +1000

    Create Week 2.py

---
+++ Week 2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 09:27:39 2024
+
+@author: marcus
+"""
+
+fname = input("What is your first name? ")
+
+num = input("Give me a number: ")
+## Change the num variable to an integer
+
+
+print("Hello " + fname)
+print("Hello", fname)
+
+print("The next number is " + (num + 1) )
+print("The next number is", num + 1)
+
+# a = 1
+# b = 2.0
+# c = "test"
+
+# print("a is", type(a))
+# print("b is", type(b))
+# print("c is", type(c))
+
+# a = float(a)
+# b = int(b)
+# #c = int(c)
+
+# print("a is", type(a))
+# print("b is", type(b))
+# print("c is", type(c))
+
