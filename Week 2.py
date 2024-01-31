966fc7fff221a5708156bfeba33b5ec5ed31bfe4
Marcus Herstik <marcus@herstik.com>
2024-01-31 13:07:37 +1000

    Update Week 2.py

--- Week 2.py
+++ Week 2.py
@@ -6,15 +6,76 @@ Created on Wed Jan 31 09:27:39 2024
 @author: marcus
 """
 
-fname = input("What is your first name? ")
+## Grade check code!
 
-num = input("Give me a number: ")
-#num = float(num)
+## Fail          0 - 49
+#   Pass        50 - 65
+#   Credit      65 - 75
+#   Distinction 75 - 85
+#   HD          85 +
 
-if fname == "Marcus":
-    print(f"Hello {fname}")
-else:
-    print("Get off my lawn")
+# Put in a score and get their grade
+# check it's a number.
+# Say their name and their grade.
+
+
+# import sys
+
+# counter =3
+# fname = input("What is your first name? ")
+
+# num = input("Give me your age: ")
+
+
+# while True:
+#     counter = counter - 1
+#     if num.isdigit():
+#         num = int(num)
+#         break
+#     else:
+#         print("That's not a number.")
+#         print(f"Chance number {counter}")
+#         #num = input("Give me your age: ")
+#         if counter > 0:
+#             num = input("Give me your age: ")
+#         else:
+#             print("Take a look at yourself!")
+#             sys.exit()
+#             # break
+#             #quit()
+#             # exit(1)
+
+# while fname != "Marcus":
+#     print("You need to be Marcus")
+#     fname = input("What is your first name? ")
+
+
+
+#num = int(input("Give me your age: "))
+
+# if fname == "Marcus":
+#     print(f"Hello {fname}")
+# else:
+#     print("Get off my lawn")
+
+## Test to see if they can enter the nightclub
+# under 18's are not allowed in
+# if num < 18:
+#     print(f"{fname} you are too young")
+# # over 18's and less than 40 pay $10
+# elif num >= 18 and num < 40:
+#     print("Pay $10")
+# # over 40's pay $25
+# elif num >= 40:
+#     # bonus if older to ask are they sure
+#     sure = input("Are you sure you want to come in (yes/no): ")
+#     sure = sure.lower()
+#     if sure == "yes" or sure == "sure" or sure == "y":
+#         print("pay $25")
+#     else: 
+#         print(f"Bye {fname}. Have a good night")
+
+
 
 ## Test if it's really a number
 # if num.isdecimal():
